f3dec9f97ac08792c37793e86bdb5dabc1ba4e5c
macsj200 <maxjohansen@berkeley.edu>
2016-08-07 17:50:22 -0700

    add ui support

--- client/main.js
+++ client/main.js
@@ -4,10 +4,25 @@ import dockerode from 'dockerode';
 
 import './main.html';
 
+Template.dockerStuff.onCreated(() => {
+	Template.instance().instanceId = new ReactiveVar('-1');
+});
+
 Template.dockerStuff.events({
 	'click button.start'(){
+		const templateInstance = Template.instance();
 		Meteor.call('startNotebook','',(err, res)=>{
-			console.log(err, res);
+			if(err){
+				console.log(err);
+			} else {
+				templateInstance.instanceId.set(res);
+			}
 		});
+	}
+});
+
+Template.dockerStuff.helpers({
+	instanceId (){
+		return Template.instance().instanceId.get();
 	}
 });
